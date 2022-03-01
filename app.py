3155f101a6f211683c6515fefa0cd2bcf1db4f3f
Sundaresan <sundaresanm@users.noreply.github.com>
2022-03-01 13:52:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, from Pull Request!"
+    return "Hello World, from Pull Request---- CHALLENGE LAB!"
 
 
 if __name__ == "__main__":
